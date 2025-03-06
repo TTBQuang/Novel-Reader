6246ca86fd5bf7a3be8478289e92a007035bcf35
21120540 <buuquang102@gmail.com>
2025-03-06 22:13:34 +0700

    config to help mobile devices be able to access website

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -5,7 +5,7 @@ interface FetchOptions extends Omit<RequestInit, 'headers'> {
     headers?: HeadersInit;
 }
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = `${window.location.protocol}//${window.location.hostname}:8080`;
 
 function normalizeHeaders(headers?: HeadersInit): Record<string, string> {
     const normalized: Record<string, string> = {};
